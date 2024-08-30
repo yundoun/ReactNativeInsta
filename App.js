f6d4ede74b2936c25e7045cbe60951ed8a797f11
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2024-08-30 17:48:15 +0900

    Feat : navigation 설정

--- App.js
+++ App.js
@@ -9,20 +9,39 @@ import Search from './src/screens/Search';
 import Activity from './src/screens/Activity';
 import { NavigationContainer } from '@react-navigation/native';
 import Status from './src/screens/Status';
+import Ionic from 'react-native-vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const getTabBarIcon = (route, focused, size, color) => {
+  let iconName;
+  color = 'black';
+  if (route.name === 'Home') {
+    iconName = focused ? 'home-sharp' : 'home-outline';
+    size = focused ? size + 8 : size + 2;
+  } else if (route.name === 'Search') {
+    iconName = focused ? 'search' : 'search-outline';
+  } else if (route.name === 'Activity') {
+    iconName = focused ? 'heart' : 'heart-outline';
+  } else if (route.name === 'Profile') {
+    iconName = focused ? 'person' : 'person-outline';
+  }
+
+  return <Ionic name={iconName} size={size} color={color} />;
+};
+
 const BottomTabScreen = () => {
   return (
     <Tab.Navigator
-      screenOptions={() => ({
+      screenOptions={({ route }) => ({
         tabBarHideOnKeyboard: true,
         tabBarShowLabel: false,
         headerShown: false,
         tabBarStyle: {
           height: 70,
         },
+        tabBarIcon: ({ focused, size, color }) => getTabBarIcon(route, focused, size, color),
       })}
     >
       <Tab.Screen name="Home" component={Home} />
--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
 const EditProfile = () => {
   return (
     <View>
       <Text>EditProfile</Text>
     </View>
-  )
-}
+  );
+};
 
-export default EditProfile
+export default EditProfile;
